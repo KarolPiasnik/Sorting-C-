e12feaf498a93c30819b8eab7e6c381309922817
Karol Piaśnik <piasnik.karol@outlook.com>
2016-12-14 08:08:24 +0100

    Let's go

---
+++ Sorting.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "stdafx.h"
+
+class Sorting 
+{
+	void bubbleSort();
+	void quickSort();
+	void bogoSort();
+	void selectionSort();
+	void insertionSort();
+	void mergeSort();
+	void heapSort();
+	void redixSort();
+	//std::sort
+	void stableSort();
+	void shellSort();
+	void coctailShakerSort();
+	void gnomeSort();
+	void bitonicSort();
+};
---
+++ sortItAll.cpp
@@ -0,0 +1,11 @@
+// sortItAll.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+//program written to practise std::array and sorting alghorithms
+
+int main()
+{
+    return 0;
+}
+
