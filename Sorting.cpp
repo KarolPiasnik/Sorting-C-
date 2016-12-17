461bab270d5ea11085ed4c711d2a3e83c226bd67
Karol Piaśnik <piasnik.karol@outlook.com>
2016-12-17 09:08:11 +0100

    gnome sort featured

--- Sorting.cpp
+++ Sorting.cpp
@@ -22,30 +22,72 @@ void Sorting::bubbleSort(std::vector<int> &a)
 	}
 }
 
-void quickSort(std::vector<int> &a)
+void Sorting::quickSort(std::vector<int> &a)
 {}
 
-void bogoSort(std::vector<int> &a) 
+void Sorting::bogoSort(std::vector<int> &a)
 {}
-void selectionSort(std::vector<int> &a) 
+void Sorting::selectionSort(std::vector<int> &a)
+{
+	std::vector<int>::iterator tmp = a.begin();
+	int min;
+	for (std::vector<int>::iterator i = a.begin(); i != a.end(); ++i)
+	{
+		min = *i;
+		for (std::vector<int>::iterator it = i /*a.begin() + (i - a.begin())*/; it != a.end(); ++it)
+		{
+			//std::cout << i - a.begin();
+			if (*it < min) 
+			{
+				min = *it;
+				tmp = it;
+			}
+		}
+
+		//using min as helping variable
+		*tmp = *i;
+		*i = min;
+	}
+}
+void Sorting::insertionSort(std::vector<int> &a)
+{
+	
+}
+void Sorting::mergeSort(std::vector<int> &a)
 {}
-void insertionSort(std::vector<int> &a) 
+void Sorting::heapSort(std::vector<int> &a)
 {}
-void mergeSort(std::vector<int> &a) 
+void Sorting::redixSort(std::vector<int> &a)
 {}
-void heapSort(std::vector<int> &a) 
+void Sorting::stableSort(std::vector<int> &a)
 {}
-void redixSort(std::vector<int> &a)
+void Sorting::shellSort(std::vector<int> &a)
 {}
-void stableSort(std::vector<int> &a) 
+void Sorting::coctailShakerSort(std::vector<int> &a)
 {}
-void shellSort(std::vector<int> &a)
+void Sorting::gnomeSort(std::vector<int> &a)
+{
+	int tmp;
+	std::vector<int>::iterator i = a.begin();
+	std::vector<int>::iterator j = a.begin() + 1;
+	while (j != a.end())
+	{
+		if (*i <= *j)
+		{
+			++i;
+			++j;
+		}
+		else
+		{
+			tmp = *i;
+			*i = *j;
+			*j = tmp;
+			if (i != a.begin()) { --i; --j; }	
+		}
+	}
+}
+
+void Sorting::bitonicSort(std::vector<int> &a)
 {}
-void coctailShakerSort(std::vector<int> &a)
-{}
-void gnomeSort(std::vector<int> &a) 
-{}
-void bitonicSort(std::vector<int> &a) 
-{}
-void gravitySort(std::vector<int> &a) 
+void Sorting::gravitySort(std::vector<int> &a)
 {}
