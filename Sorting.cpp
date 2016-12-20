dcc4bde872323b7445f9178dab115a58897e3eab
Karol Piaśnik <piasnik.karol@outlook.com>
2016-12-20 21:09:38 +0100

    starting gravity sort implementation

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "Sorting.h"
 #include <iostream>
+#include <array>
+
 void Sorting::bubbleSort(std::vector<int> &a) 
 {
 	int tmp = 0;
@@ -120,5 +122,43 @@ void Sorting::gnomeSort(std::vector<int> &a)
 
 void Sorting::bitonicSort(std::vector<int> &a)
 {}
+
 void Sorting::gravitySort(std::vector<int> &a)
-{}
+{
+	int max = a.at(0);
+	bool ** data = new bool*[a.size()]; // for storing numbers 
+
+
+	for (std::vector<int>::iterator it = a.begin(); it != a.end(); ++it) // finding maximum
+	{
+		if (*it > max)
+			max = *it;
+	}
+
+	for (int i = 0; i < a.size(); ++i)
+	{
+		data[i] = new bool[max];
+		for (int j = 0; j < max; ++j)
+		{
+			if (a.at(i) != 0)
+			{
+				data[i][j] = true;
+				--a.at(0);
+			}
+			else
+				data[i][j] = false;
+		}
+	}
+
+	for (int i = 0; i < a.size(); ++i)
+	{
+		a.at(i) = 0;
+		for (int j = 0; j < max; ++j)
+			if (data[i][j])
+				++a.at(i);
+	}
+
+
+
+
+}
